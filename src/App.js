113aef607f4ee673c584610957d1ccec411f3593
AhMi <mohammadahmad441@gmail.com>
2022-05-02 17:43:46 +0500

    desktop Bounties completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Bounties from "./Components/Bounties";
+
+// code bugs on localhost but works fine on production.. i'll fix this asap
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Bounties />
+    </>
   );
 }
 
